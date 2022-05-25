428b7deb506f53242ac0139e6ba1e3326de9dfee
Maja Wirkijowska <maja.wirkijowska@gmail.com>
2022-05-25 08:14:49 -0700

    project created

---
+++ src/test/java/tddByExample/TddByExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package tddByExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddByExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
